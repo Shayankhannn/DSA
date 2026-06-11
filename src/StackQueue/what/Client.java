dcf1e9dd0296d6d61f820e8b92a89ce2676cf0d9
shayankhannn <shayannkhannn007@gmail.com>
2026-06-11 08:49:06 -0700

    Stack and queue

---
+++ src/StackQueue/what/Client.java
@@ -0,0 +1,11 @@
+//package StackQueue.what;
+//
+//public class Client {
+//    StackOfStrings stack = new StackOfStrings();
+// while (!StdIn.isEmpty())
+//    {
+//        String s = StdIn.readString();
+//        if (s.equals("-")) StdOut.print(stack.pop());
+//        else stack.push(s);
+//    }
+//}
---
+++ src/StackQueue/what/StackAPI.java
@@ -0,0 +1,10 @@
+//package StackQueue.what;
+//
+//public class StackAPI {
+//    StackOfStrings() create an empty stack
+//    void push(String item) insert a new string onto stack
+//    String pop() remove and return the string
+//    most recently added
+//    boolean isEmpty() is the stack empty?
+//    int size() number of strings on the stack
+//}
