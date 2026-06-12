33a72b8c3cc25f9285fc716b5f6dbfcb49bfda13
shayankhannn <shayannkhannn007@gmail.com>
2026-06-12 05:35:11 -0700

    Stack and queue -- queues api linkelist queues

---
+++ src/StackQueue/QueueLinkedList.java
@@ -0,0 +1,27 @@
+package StackQueue;
+
+public class QueueLinkedList {
+    private Node first,last;
+    private class Node{
+        String item;
+        Node next;
+    }
+    public boolean isEmpty (){
+        return first == null;
+    }
+    public void enqueue(String item){
+        Node oldlast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else oldlast.next = last;
+    }
+    public String dequeue(){
+        String item = first.item;
+        first = first.next;
+        first.item = null;    // Avoid loitering
+        if (isEmpty()) last = null;
+        return  item;
+    }
+}
---
+++ src/StackQueue/what/QueueApi.java
@@ -0,0 +1,10 @@
+package StackQueue.what;
+
+public class QueueApi {
+//    QueueOfStrings() create an empty queue
+//    void enqueue(String item) insert a new string onto queue
+//    String dequeue() remove and return the string
+//    least recently added
+//    boolean isEmpty() is the queue empty?
+//    int size() number of strings on the queue
+}
